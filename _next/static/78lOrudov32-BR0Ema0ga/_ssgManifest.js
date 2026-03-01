29c45fff9bd9fddd9dacd412a907ab8ee176d9e4
José Pedro Schardosim Simão <josepedrosimao@gmail.com>
2026-03-01 14:13:50 -0300

    Deploy: 2026-03-01 14:12:51

---
+++ _next/static/78lOrudov32-BR0Ema0ga/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fblog","\u002F[locale]\u002Fblog\u002F[slug]","\u002F[locale]\u002Fcv","\u002F[locale]\u002Fpublications","\u002F[locale]\u002Fstack"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
